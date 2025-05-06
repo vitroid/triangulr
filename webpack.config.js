0af282ba080122760fcfb9c3bd2b8a7f5544560c
vitroid <vitroid@gmail.com>
2025-05-06 13:44:19 +0900

    Add publicPath for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: process.env.NODE_ENV === "production" ? "/triangulr/" : "/",
   },
   module: {
     rules: [
